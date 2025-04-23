f9d3a8d23bc32c0501d2a15f12e21d74d43d9099
Salvador Guzman <guzmansalv@gmail.com>
2025-04-23 13:22:54 -0700

    Implement paragraph translation from French to English using rust-bert

---
+++ src/pipeline.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+use log::{debug, warn};
+use rust_bert::pipelines::translation::{Language, TranslationModelBuilder};
+
+pub fn translate_paragraphs(paragraphs: &[String]) -> Result<Vec<String>> {
+    let model = TranslationModelBuilder::new()
+        .with_source_languages(vec![Language::French])
+        .with_target_languages(vec![Language::English])
+        .create_model()?;
+
+    let mut results = vec![];
+    for chunk in paragraphs.chunks(4) {
+        debug!("Translating chunk of size {}", chunk.len());
+        let translations = model.translate(chunk, None, Language::French, Language::English)?;
+        results.extend(translations);
+    }
+
+    Ok(results)
+}
